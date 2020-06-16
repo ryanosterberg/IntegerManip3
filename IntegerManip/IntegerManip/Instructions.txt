617ef26009a35a0bd2ca347aabe34baf9bf4286d
Mr O <ryan.p.osterberg@gmail.com>
2020-06-16 14:45:58 -0500

    Project Creation

---
+++ IntegerManip/IntegerManip/Instructions.txt
@@ -0,0 +1,15 @@
+﻿Source Control Activity #5 - Integer Manipulation
+
+As a team, write a project to prompt the user for up to 20 integers.  The user will be allowed to choose from the following menu options until they decide to quit:
+
+	• Display the whole list of input
+	• Display every even number
+	• Display every odd number
+	• Display every 5th number
+	• Display the sum of the integers
+	• Display every number divisible by 3
+	• Quit
+
+Team - Be sure to use proper source control techniques to manage pull requests
+
+Include additional features time permitting.
---
+++ IntegerManip/IntegerManip/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerManip
+{
+    class Program
+    {
+        //As a team, write a project to prompt the user for up to 20 integers.  
+        //The user will be allowed to choose from the following menu options until they decide to quit:
+        //• Display the whole list of input
+        //• Display every even number
+        //• Display every odd number
+        //• Display every 5th number
+        //• Display the sum of the integers
+        //• Display every number divisible by 3
+        //• Quit
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
